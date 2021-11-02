e4ba630985f74a8aaf78b4d024e3d3485bce991e
Nathan Malicki <43974544+ntm5579@users.noreply.github.com>
2021-11-01 21:02:45 -0400

    Add files via upload

--- src/Blurrer.py
+++ src/Blurrer.py
@@ -10,39 +10,37 @@ im_new = Image.new('RGB', (width, height))
 def blurPixel(i, j):
     rAvg, gAvg, bAvg = 0, 0, 0
 
-    xrangeList = [-1,1]
-    yrangeList = [-1,1]
+    xrangeList = [-1,2]
+    yrangeList = [-1,2]
     if(i == 0):
-        xrangeList = [0,1]
+        xrangeList = [0,2]
     if(j == 0):
-        yrangeList = [0,1]
+        yrangeList = [0,2]
     if(i == width):
         xrangeList = [-1,0]
     if(j == height):
         yrangeList = [-1,0]
-    
-    for x_offset in range(xrangeList[0],xrangeList[1]):
-        for y_offset in range(yrangeList[0],yrangeList[1]):
-            targetPixel = im.getpixel((i+ x_offset, j+ y_offset))
+
+    for x_offset in range(xrangeList[0], xrangeList[1]):
+        for y_offset in range(yrangeList[0], yrangeList[1]):
+            try:
+                targetPixel = im.getpixel((i+ x_offset, j+ y_offset))
+            except IndexError:
+                print('i', i , 'j', j)
+                exit()
             rAvg += targetPixel[0]
             gAvg += targetPixel[1]
             bAvg += targetPixel[2]
-    self_sub = im.getpixel((i, j))
-    rAvg -= self_sub[0]
-    gAvg -= self_sub[1]
-    bAvg -= self_sub[2]
 
     #divide by 8 because we are not taking into account the color of that pixel
-    blurred_pixel = [rAvg//8, gAvg//8, bAvg//8]
+    blurred_pixel = [rAvg//9, gAvg//9, bAvg//9]
     blurred_pixel = tuple(blurred_pixel)
-    #print(blurred_pixel)
     return blurred_pixel
 
 #print(width, height)
-for i in range(width):
-    for j in range(height):
+for i in range(width - 1):
+    for j in range(height - 1):
         im_new.putpixel( (i, j), blurPixel(i, j) )
-    #print("Row", i, "is done")
     loadBar(iteration=i, total = width)
 
 im_new.show()
