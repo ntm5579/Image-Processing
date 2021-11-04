be0662ae001922c2841add62450a7069fe56f5e3
Nathan Malicki <43974544+ntm5579@users.noreply.github.com>
2021-11-04 02:40:12 -0400

    Add files via upload

--- Functions.py
+++ Functions.py
@@ -2,73 +2,52 @@ from random import randint
 from PIL import Image
 
 #do not call without fixing input
-#makes a square of rgb or bw noise size by size in dimensions.
-def NoiseGenerator(size, rgb = False):
+def NoiseGenerator(size, rgb = False): #makes a square of rgb or bw noise size by size in dimensions.
     #determines what mode the image is created in
-    if rgb:
-        #rgb mode
+    if rgb: #rgb mode
         im = Image.new('RGB', (size,size))
-    else:
-        #grayscale mode
+    else: #grayscale mode
         im = Image.new('L', (size,size))
 
-    for i in range(size):
-        for j in range(size):
-            #rgb mode
-            if rgb:
-                #places pixels in new image with a randomized rgb values
-                im.putpixel( (i, j), (randint(0,255), randint(0,255), randint(0,255)))
-            #grayscale mode
-            else:
-                #places pixels in new image with a randomized w value
-                im.putpixel( (i, j), randint(0,255))
-    #diplays the finished image in photos app
-    im.show()
+    for i in range(size): #loops through x values
+        for j in range(size): #loops through y values
+            if rgb: #rgb mode
+                im.putpixel( (i, j), (randint(0,255), randint(0,255), randint(0,255))) #places pixels in new image with a randomized rgb values
+            else: #grayscale mode
+                im.putpixel( (i, j), randint(0,255)) #places pixels in new image with a randomized bw value
+    im.show() #diplays the finished image in photos app
 
-#devitates each pixel rgb value a random amount from the original image
-def Scrambler(filePath):
-    #opens and assigns the image selected to im
-    with Image.open(filePath).convert('RGB') as im:
+def Scrambler(filePath): #devitates each pixel rgb value a random amount from the original image
+    with Image.open(filePath).convert('RGB') as im: #opens and assigns the image selected to im
         width, height = im.size
         #shows the image, needed for init rn, would like to remove
-        #marked for removal
-        im.show()
+        im.show()#marked for removal
     
-    #loops through x values
-    for i in range(width):
-        #loops through y values
-        for j in range(height):
-            #stores the rgb values of the current pixel being worked on
-            pixel = im.getpixel( (i, j) )
+    for i in range(width): #loops through x values
+        for j in range(height): #loops through y values
+            pixel = im.getpixel( (i, j) )#stores the rgb values of the current pixel being worked on
             randomized_pixel = []
-            #generates a random value for each color channel to deviate from the original pixel
-            for h in range(3):
+            for h in range(3): #generates a random value for each color channel to deviate from the original pixel
                 random_value = randint(-255,255)
-                #makes sure the new pixel value is displayable in rgb format (0-255), rerandomizes if not
-                while pixel[h] + random_value < 255 and pixel[h] + random_value > 0:
+                while pixel[h] + random_value < 255 and pixel[h] + random_value > 0: #makes sure the new pixel value is displayable in rgb format (0-255), rerandomizes if not
                     random_value = randint(-255, 255)
                 randomized_pixel.append(pixel[h] + random_value)
-            #converts the randomized pixel to tuple to be placed on the new image
-            randomized_pixel = tuple(randomized_pixel)
+            randomized_pixel = tuple(randomized_pixel) #converts the randomized pixel to tuple to be placed on the new image
             im.putpixel( (i, j), randomized_pixel )
-    im.show()
+    im.show() #diplays the finished image in photos app
 
-#makes a negative of the uploaded file
-def makeNegative(filePath):
+def makeNegative(filePath): #makes a negative of the uploaded file
     with Image.open(filePath).convert('RGB') as im:
         width, height = im.size
-        #marked for removal
-        im.show()
+        im.show() #marked for removal
 
-    for i in range(width):
-        for j in range(height):
+    for i in range(width): #loops through x values
+        for j in range(height): #loops through y values
             pixel = im.getpixel( (i, j) )
-            #flips the pixel value by subtracting from max color value
-            negativePixel = (255-pixel[0], 255-pixel[1], 255-pixel[2])
+            negativePixel = (255-pixel[0], 255-pixel[1], 255-pixel[2]) #flips the pixel value by subtracting from max color value
             im.putpixel( (i, j), negativePixel)
+    im.show() #diplays the finished image in photos app
 
-    im.show()
-
-#add blurrer when it is finished
 def Blur(filePath):
+    #add body of blurrer when it is finished
     print('Will blur Filepath', filePath, "when the method works")
--- GUI.py
+++ GUI.py
@@ -10,47 +10,50 @@ def loadAndUnload(menuToLoad):
     for menu in menuList:
         if menu != menuToLoad:
             menu.grid_forget()
-    menuToLoad.grid(column = 0, row= 0)
+    menuToLoad.grid(column = 0, row= 0, padx= 15, pady = 5)
 
 window = Tk()
 window.geometry('800x600')
 window.title("Gui Test")
-window.resizable(False, False) 
+window.resizable(False, False)
+window.config(bg='dark gray')
 
 #---------------------------------------------------------------------------
-#holds the home and back button, displayed most of the time
-universalFrame = Frame(window, width= 30, height = 2, bg= 'aqua')
-universalFrame.grid(column= 1, row = 0, sticky= "NE")
+universalFrame = Frame(window, width= 100, height = 2, bg= 'aqua') #holds the home and back button, displayed most of the time
+universalFrame.grid(column= 0, row = 0, padx= 15, pady = 5, columnspan=3)
+
+homeButton = Button(universalFrame, text= "Home", width = 10, height = 2, command=lambda: loadAndUnload(mainMenu))
+homeButton.grid(column=0, row= 0, padx= 5, pady = 5)
 
 #loads the last frame and then takes that item off of the list of pages, does not work rn, watch for minus two indexing issues
 backButton = Button(universalFrame, text= "Back", width = 15, height = 2, command=lambda: loadAndUnload(previousWindow[-2]))
-backButton.grid(column=0, row= 0, padx= 5, pady = 5)
+backButton.grid(column=1, row= 0, padx= 5, pady = 5)
 
-homeButton = Button(universalFrame, text= "Home", width = 10, height = 2, command=lambda: loadAndUnload(mainMenu))
-homeButton.grid(column=1, row= 0, padx= 5, pady = 5)
+settingsButton = Button(universalFrame, text= "Settings", width = 10, height = 2)
+settingsButton.grid(column=2, row= 0, padx= 5, pady = 5)
 
 exitButton = Button(universalFrame, text= "Exit", width = 10, height = 2, command=window.quit)
-exitButton.grid(column=1, row= 1, padx= 5, pady = 5)
+exitButton.grid(column=3, row= 0, padx= 5, pady = 5)
 #---------------------------------------------------------------------------
 
 #the starting menu that lets you get to the create and upload pages
-mainMenu = Frame(window)
-mainMenu.grid(column = 0, row= 0, padx= 15)
+mainMenu = Frame(window, bg= 'white')
+mainMenu.grid(column = 0, row= 1, padx= 15, pady=5)
 menuList.append(mainMenu)
 
 previousWindow = [mainMenu]
 
 menuLabel = Label(mainMenu, text="Menu", width= 65, height= 2, bg = "Coral")
-menuLabel.grid(column= 0, row= 0, padx= 10, columnspan=2)
+menuLabel.grid(column= 0, row= 0, padx= 5, pady = 5, columnspan=2)
 
 uploadButton = Button(mainMenu, text= "Upload your own image", width = 20, height = 4, command= lambda:loadAndUnload(uploadMenu))
 uploadButton.grid(column = 0, row= 1, padx= 5, pady = 10)
 
-uploadMenu = Frame(window)
+uploadMenu = Frame(window, bg= 'white')
 menuList.append(uploadMenu)
 
 uploadLabel = Label(uploadMenu, text = "Uploading and shit", width= 65, height=2, bg = "Coral")
-uploadLabel.grid(column = 0, row= 0, padx= 10, columnspan=2)
+uploadLabel.grid(column = 0, row= 0, padx= 5, pady= 5, columnspan=2)
 
 filename = ''
 def imageUpload():
@@ -83,14 +86,14 @@ scrambleButton = Button(uploadMenu, text= "Scramble this image", state="disabled
 scrambleButton.grid(column= 1, row = 2, padx= 5, pady = 5)
 
 
-createMenu = Frame(window)
+createMenu = Frame(window, bg= 'white')
 menuList.append(createMenu)
 
 createButton = Button(mainMenu, text="Create a new image", width = 20, height = 4, command=lambda: loadAndUnload(createMenu))
 createButton.grid(column = 1, row= 1, padx= 5)
 
 createLabel = Label(createMenu, text = "Creating and shit", width= 65, height= 2, bg = "coral")
-createLabel.grid(column = 0, row= 0, padx= 5, columnspan=2)
+createLabel.grid(column = 0, row= 0, padx= 5, pady = 5, columnspan=2)
 
 numLabel = Label(createMenu, text= "Enter the size of square you would like to generate")
 numLabel.grid(column= 0, row = 1, padx= 5, pady = 5, columnspan=2)
