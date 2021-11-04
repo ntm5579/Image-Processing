c249af636a20ab23df5e4e3e4af97d58524396c8
Nathan Malicki <43974544+ntm5579@users.noreply.github.com>
2021-11-04 02:17:42 -0400

    Add files via upload

--- Functions.py
+++ Functions.py
@@ -2,6 +2,7 @@ from random import randint
 from PIL import Image
 
 #do not call without fixing input
+#makes a square of rgb or bw noise size by size in dimensions.
 def NoiseGenerator(size, rgb = False):
     #determines what mode the image is created in
     if rgb:
@@ -9,51 +10,65 @@ def NoiseGenerator(size, rgb = False):
         im = Image.new('RGB', (size,size))
     else:
         #grayscale mode
-        im = Image.new('L', (size,size)) # create the Image of size 1 pixel
+        im = Image.new('L', (size,size))
 
     for i in range(size):
         for j in range(size):
+            #rgb mode
             if rgb:
-                #rgb mode
+                #places pixels in new image with a randomized rgb values
                 im.putpixel( (i, j), (randint(0,255), randint(0,255), randint(0,255)))
+            #grayscale mode
             else:
-                #grayscale mode
+                #places pixels in new image with a randomized w value
                 im.putpixel( (i, j), randint(0,255))
+    #diplays the finished image in photos app
     im.show()
 
-#handle image input, might have to 
+#devitates each pixel rgb value a random amount from the original image
 def Scrambler(filePath):
+    #opens and assigns the image selected to im
     with Image.open(filePath).convert('RGB') as im:
         width, height = im.size
+        #shows the image, needed for init rn, would like to remove
+        #marked for removal
         im.show()
-    width, height = im.size
+    
+    #loops through x values
     for i in range(width):
-        #print("Row", i, "is done")
+        #loops through y values
         for j in range(height):
+            #stores the rgb values of the current pixel being worked on
             pixel = im.getpixel( (i, j) )
             randomized_pixel = []
+            #generates a random value for each color channel to deviate from the original pixel
             for h in range(3):
                 random_value = randint(-255,255)
+                #makes sure the new pixel value is displayable in rgb format (0-255), rerandomizes if not
                 while pixel[h] + random_value < 255 and pixel[h] + random_value > 0:
                     random_value = randint(-255, 255)
                 randomized_pixel.append(pixel[h] + random_value)
+            #converts the randomized pixel to tuple to be placed on the new image
             randomized_pixel = tuple(randomized_pixel)
             im.putpixel( (i, j), randomized_pixel )
-        #loadBar(iteration=i, total = width)
     im.show()
 
+#makes a negative of the uploaded file
 def makeNegative(filePath):
     with Image.open(filePath).convert('RGB') as im:
         width, height = im.size
+        #marked for removal
         im.show()
 
     for i in range(width):
         for j in range(height):
             pixel = im.getpixel( (i, j) )
+            #flips the pixel value by subtracting from max color value
             negativePixel = (255-pixel[0], 255-pixel[1], 255-pixel[2])
             im.putpixel( (i, j), negativePixel)
-        #loadBar(i, width)
 
     im.show()
 
 #add blurrer when it is finished
+def Blur(filePath):
+    print('Will blur Filepath', filePath, "when the method works")
--- GUI.py
+++ GUI.py
@@ -1,85 +1,135 @@
 from tkinter import *
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, test
 from Functions import *
+from PIL import ImageTk, Image
 
-def GUI_Init():
-    menuList = []
 
-    def loadAndUnload(menuToLoad):
-        for menu in menuList:
-            if menu != menuToLoad:
-                menu.grid_forget()
-        menuToLoad.grid(column = 0, row= 0)
+menuList = []
 
-    window = Tk()
-    window.geometry('800x600')
-    window.title("Gui Test")
-    window.resizable(False, False) 
+def loadAndUnload(menuToLoad):
+    for menu in menuList:
+        if menu != menuToLoad:
+            menu.grid_forget()
+    menuToLoad.grid(column = 0, row= 0)
 
-    universalFrame = Frame(window, width= 30, height = 2, bg= 'aqua')
-    universalFrame.grid(column= 1, row = 0, sticky= "NE")
+window = Tk()
+window.geometry('800x600')
+window.title("Gui Test")
+window.resizable(False, False) 
 
-    #loads the last window and then takes that item off of the list of pages, watch the minus two indexing issus
-    backButton = Button(universalFrame, text= "Back", width = 15, height = 2, command=lambda: loadAndUnload(previousWindow[-2]))
-    backButton.grid(column=0, row= 0, padx= 5, pady = 5)
+#---------------------------------------------------------------------------
+#holds the home and back button, displayed most of the time
+universalFrame = Frame(window, width= 30, height = 2, bg= 'aqua')
+universalFrame.grid(column= 1, row = 0, sticky= "NE")
 
-    homeButton = Button(universalFrame, text= "Home", width = 10, height = 2, command=lambda: loadAndUnload(mainMenu))
-    homeButton.grid(column=1, row= 0, padx= 5, pady = 5)
+#loads the last frame and then takes that item off of the list of pages, does not work rn, watch for minus two indexing issues
+backButton = Button(universalFrame, text= "Back", width = 15, height = 2, command=lambda: loadAndUnload(previousWindow[-2]))
+backButton.grid(column=0, row= 0, padx= 5, pady = 5)
 
-    mainMenu = Frame(window)
-    mainMenu.grid(column = 0, row= 0, padx= 15)
-    menuList.append(mainMenu)
+homeButton = Button(universalFrame, text= "Home", width = 10, height = 2, command=lambda: loadAndUnload(mainMenu))
+homeButton.grid(column=1, row= 0, padx= 5, pady = 5)
 
-    previousWindow = [mainMenu]
+exitButton = Button(universalFrame, text= "Exit", width = 10, height = 2, command=window.quit)
+exitButton.grid(column=1, row= 1, padx= 5, pady = 5)
+#---------------------------------------------------------------------------
 
-    menuLabel = Label(mainMenu, text="Menu", width= 65, height= 2, bg = "Coral")
-    menuLabel.grid(column= 0, row= 0, padx= 10, columnspan=2)
+#the starting menu that lets you get to the create and upload pages
+mainMenu = Frame(window)
+mainMenu.grid(column = 0, row= 0, padx= 15)
+menuList.append(mainMenu)
 
-    uploadButton = Button(mainMenu, text= "Upload your own image", width = 20, height = 4, command= lambda:loadAndUnload(uploadMenu))
-    uploadButton.grid(column = 0, row= 1, padx= 5, pady = 10)
+previousWindow = [mainMenu]
 
-    uploadMenu = Frame(window)
-    menuList.append(uploadMenu)
+menuLabel = Label(mainMenu, text="Menu", width= 65, height= 2, bg = "Coral")
+menuLabel.grid(column= 0, row= 0, padx= 10, columnspan=2)
 
-    uploadLabel = Label(uploadMenu, text = "Uploading and shit", width= 65, height=2, bg = "Coral")
-    uploadLabel.grid(column = 0, row= 0, padx= 10, columnspan=2)
+uploadButton = Button(mainMenu, text= "Upload your own image", width = 20, height = 4, command= lambda:loadAndUnload(uploadMenu))
+uploadButton.grid(column = 0, row= 1, padx= 5, pady = 10)
 
-    filename = ''
-    def imageUpload():
-        global filename
-        Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-        filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-        print(filename)
-        #might have to global this shit
+uploadMenu = Frame(window)
+menuList.append(uploadMenu)
+
+uploadLabel = Label(uploadMenu, text = "Uploading and shit", width= 65, height=2, bg = "Coral")
+uploadLabel.grid(column = 0, row= 0, padx= 10, columnspan=2)
+
+filename = ''
+def imageUpload():
+    global filename
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filepath = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    if filepath != '':
+        #print(filepath)
+        blurButton.config(state='normal')
+        scrambleButton.config(state='normal')
         #make this a preview of upload
-        uploadedImageDisplay = ''
+        uploadedImageDisplay = Frame(uploadMenu, bg= 'aqua')
+        uploadedImageDisplay.grid(column= 1, row = 1, padx= 5, pady = 5)
+        fileNameLabel = Label(uploadedImageDisplay, text = filepath[:15] + '...' + filepath[-15:-1])
+        fileNameLabel.grid(column= 0, row = 0)
+        im = ImageTk.PhotoImage(Image.open(filepath))
+        thumbNailIm = Canvas(uploadedImageDisplay, height= 100, width= 100)
+        thumbNailIm.create_image(20, 20, image = im)
+        thumbNailIm.grid(column= 0, row = 1)
 
-    #set up command to get a file path to work with, have this function enable the blurbutton and scramble button
-    uploadImageButton = Button(uploadMenu, text= "Click hear to upload \n an Image", width = 20, height = 4, command=imageUpload)
-    uploadImageButton.grid(column= 0, row = 1, columnspan=2, padx= 5, pady = 5)
+#set up command to get a file path to work with, have this function enable the blurbutton and scramble button
+uploadImageButton = Button(uploadMenu, text= "Upload an Image", width = 15, height = 2, command=imageUpload)
+uploadImageButton.grid(column= 0, row = 1, padx= 5, pady = 5)
 
-    #hook this up to the blur func and pass argument of file path, enable when you get a file path
-    blurButton = Button(uploadMenu, text= "Click hear to upload \n an Image", state="disabled", width = 20, height = 4)
-    blurButton.grid(column= 0, row = 2, padx= 5, pady = 5)
+#hook this up to the blur func and pass argument of file path, enable when you get a file path
+blurButton = Button(uploadMenu, text= "Blur this image", state="disabled", width = 20, height = 4, command= lambda:Blur(filename))
+blurButton.grid(column= 0, row = 2, padx= 5, pady = 5)
 
-    scrambleButton = Button(uploadMenu, text= "Click hear to upload \n an Image", state="disabled", width = 20, height = 4)
-    scrambleButton.grid(column= 1, row = 2, padx= 5, pady = 5)
-    
+scrambleButton = Button(uploadMenu, text= "Scramble this image", state="disabled", width = 20, height = 4, command= lambda:Scrambler(filename))
+scrambleButton.grid(column= 1, row = 2, padx= 5, pady = 5)
 
-    createMenu = Frame(window)
-    menuList.append(createMenu)
 
-    createButton = Button(mainMenu, text="Create a new image", width = 20, height = 4, command=lambda: loadAndUnload(createMenu))
-    createButton.grid(column = 1, row= 1, padx= 5)
+createMenu = Frame(window)
+menuList.append(createMenu)
 
-    createLabel = Label(createMenu, text = "Creating and shit", width= 65, height= 2, bg = "coral")
-    createLabel.grid(column = 0, row= 0, padx= 5)
+createButton = Button(mainMenu, text="Create a new image", width = 20, height = 4, command=lambda: loadAndUnload(createMenu))
+createButton.grid(column = 1, row= 1, padx= 5)
 
-    noiseButton = Button(createMenu, text= "Make Some Noise", width = 60, height = 4)
-    noiseButton.grid(column= 0, row = 2, padx= 5, pady = 5)
+createLabel = Label(createMenu, text = "Creating and shit", width= 65, height= 2, bg = "coral")
+createLabel.grid(column = 0, row= 0, padx= 5, columnspan=2)
 
-    window.mainloop()
+numLabel = Label(createMenu, text= "Enter the size of square you would like to generate")
+numLabel.grid(column= 0, row = 1, padx= 5, pady = 5, columnspan=2)
 
-#prevents this Gui_Init from getting called on import of loadBar()
-if __name__ == "__main__":
-    GUI_Init()
+numField = Entry(createMenu)
+numField.grid(column= 0, row = 2, padx= 5, pady = 5, columnspan=2)
+
+numWarningLabel = Label(createMenu, text= "That is not a number")
+
+def testInput(numFieldInput):
+    if(numFieldInput.isdecimal()):
+        NoiseGenerator(int(numFieldInput), rgb)
+        print(rgb)
+    else:
+        numWarningLabel.grid(column= 0, row = 3, padx= 5, pady = 5, columnspan=2)
+
+bwRgbLabel = Label(createMenu, text= "Would you like Black and White Noise or Noise with all colors?")
+bwRgbLabel.grid(column= 0, row = 4, padx= 5, pady = 5, columnspan=2)
+
+rgb = False
+
+def switchBwRgb(mode):
+    global rgb
+    if mode == 'rgb':
+        bwButton.config(state= 'normal')
+        rgbButton.config(state= 'disabled')
+        rgb = True
+    else:    
+        bwButton.config(state='disabled')
+        rgbButton.config(state= 'normal')
+        rgb = False
+
+bwButton = Button(createMenu, text= "Black and White", width = 15, height = 2, state='disabled', command= lambda: switchBwRgb('bw'))
+bwButton.grid(column= 0, row = 5, padx= 5, pady = 5)
+
+rgbButton = Button(createMenu, text= "Rgb", width = 15, height = 2, command= lambda: switchBwRgb('rgb'))
+rgbButton.grid(column= 1, row = 5, padx= 5, pady = 5)
+
+noiseButton = Button(createMenu, text= "Make Some Noise", width = 60, height = 4, command= lambda: testInput(numField.get()))
+noiseButton.grid(column= 0, row = 6, padx= 5, pady = 5, columnspan=2)
+
+window.mainloop()
