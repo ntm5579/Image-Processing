642270c771c74a1e5faa1cc77c0070ceee4e8e81
Nathan Malicki <43974544+ntm5579@users.noreply.github.com>
2021-11-04 23:56:18 -0400

    Add files via upload

--- Functions.py
+++ Functions.py
@@ -1,5 +1,32 @@
+from pathlib import WindowsPath
 from random import randint
 from PIL import Image
+from math import sqrt
+
+def rinseAndOpen(filepath):
+    #print("Before: ", filepath)
+    #make it so all escape characters have been removed
+    for i in range(len(filepath)):
+        #print('index:', i, 'Char:', filepath[i])
+        if(filepath[i] == "\\"):
+            filepath = filepath[:i] + '/' + filepath[i + 1:]
+        elif(filepath[i] == "\n"):
+            filepath = filepath[:i] + '/n' + filepath[i + 1:]
+        elif(filepath[i] == "\r"):
+            filepath = filepath[:i] + '/r' + filepath[i + 1:]
+        elif(filepath[i] == "\t"):
+            filepath = filepath[:i] + '/t' + filepath[i + 1:]
+        elif(filepath[i] == "\b"):
+            filepath = filepath[:i] + '/b' + filepath[i + 1:]
+        elif(filepath[i] == "\f"):
+            filepath = filepath[:i] + '/f' + filepath[i + 1:]
+        elif(filepath[i] == "\a"):
+            filepath = filepath[:i] + '/a' + filepath[i + 1:]
+    #print("After:", filepath)
+    with Image.open(r"{s}".format(s = filepath)).convert('RGB') as im: #opens and assigns the image selected to im
+        #shows the image, needed for init rn, would like to remove
+        im.show()#marked for removal
+    return im
 
 #do not call without fixing input
 def NoiseGenerator(size, rgb = False): #makes a square of rgb or bw noise size by size in dimensions.
@@ -17,12 +44,47 @@ def NoiseGenerator(size, rgb = False): #makes a square of rgb or bw noise size b
                 im.putpixel( (i, j), randint(0,255)) #places pixels in new image with a randomized bw value
     im.show() #diplays the finished image in photos app
 
+def NoiseClusters(size, r = 10, numClusters = -1): #makes a square of rgb or bw noise size by size in dimensions.
+    hotspots = []
+    if(numClusters < 0):
+        for _ in range(randint(5,20)):
+            newHotSpot = (randint(0,size), randint(0,size))
+            while hotspots.count(newHotSpot) > 0:
+                newHotSpot = (randint(0,size), randint(0,size))
+            hotspots.append(newHotSpot)
+    else:
+        for _ in range(numClusters):
+            newHotSpot = (randint(0,size), randint(0,size))
+            while hotspots.count(newHotSpot) > 0:
+                newHotSpot = (randint(0,size), randint(0,size))
+            hotspots.append(newHotSpot)
+    #print(hotspots)
+
+    im = Image.new('L', (size,size))
+    for i in range(size):
+        for j in range(size):
+            im.putpixel( (i, j), (255) )
+    #add a way to check if points are in the boundaries of other clusters, avearge the values then
+    overlap = []
+    for points in hotspots:
+        for x_offset in range(-r,r):
+            for y_offset in range(-r,r):
+                distance = int(sqrt((x_offset) ** 2 + (y_offset) ** 2))
+                if(distance < r):
+                    color = (int(distance/sqrt( r**2 + r**2) * 255))
+                    try:
+                        im.putpixel( (points[0] + x_offset, points[1] + y_offset), color )
+                    except IndexError:
+                        break
+
+    im.show()
+            
+#NoiseClusters(500, r = 100)
+
+#open does not work when taking full file path from windows file select
 def Scrambler(filePath): #devitates each pixel rgb value a random amount from the original image
-    with Image.open(filePath).convert('RGB') as im: #opens and assigns the image selected to im
-        width, height = im.size
-        #shows the image, needed for init rn, would like to remove
-        im.show()#marked for removal
-    
+    im = rinseAndOpen(filePath)
+    width, height = im.size
     for i in range(width): #loops through x values
         for j in range(height): #loops through y values
             pixel = im.getpixel( (i, j) )#stores the rgb values of the current pixel being worked on
@@ -37,10 +99,8 @@ def Scrambler(filePath): #devitates each pixel rgb value a random amount from th
     im.show() #diplays the finished image in photos app
 
 def makeNegative(filePath): #makes a negative of the uploaded file
-    with Image.open(filePath).convert('RGB') as im:
-        width, height = im.size
-        im.show() #marked for removal
-
+    im = rinseAndOpen(filePath)
+    width, height = im.size
     for i in range(width): #loops through x values
         for j in range(height): #loops through y values
             pixel = im.getpixel( (i, j) )
@@ -49,5 +109,7 @@ def makeNegative(filePath): #makes a negative of the uploaded file
     im.show() #diplays the finished image in photos app
 
 def Blur(filePath):
+    im = rinseAndOpen(filePath)
+    width, height = im.size
     #add body of blurrer when it is finished
     print('Will blur Filepath', filePath, "when the method works")
--- GUI.py
+++ GUI.py
@@ -6,14 +6,26 @@ from PIL import ImageTk, Image
 
 menuList = []
 
-def loadAndUnload(menuToLoad):
+def loadAndUnload(menuToLoad, remove = False):
+    global previousWindow
+    if(remove == False):
+        previousWindow.append(menuToLoad)
+    else:
+        previousWindow.pop(-2)
+    
+    if(menuToLoad == mainMenu):
+        previousWindow = [mainMenu]
+        backButton.config(state='disable')
+    else:
+        backButton.config(state='normal')
+    
     for menu in menuList:
         if menu != menuToLoad:
             menu.grid_forget()
-    menuToLoad.grid(column = 0, row= 0, padx= 15, pady = 5)
+    menuToLoad.grid(column = 0, row= 1, padx= 15, pady = 5)
 
 window = Tk()
-window.geometry('800x600')
+window.geometry('500x400')
 window.title("Gui Test")
 window.resizable(False, False)
 window.config(bg='dark gray')
@@ -26,11 +38,11 @@ homeButton = Button(universalFrame, text= "Home", width = 10, height = 2, comman
 homeButton.grid(column=0, row= 0, padx= 5, pady = 5)
 
 #loads the last frame and then takes that item off of the list of pages, does not work rn, watch for minus two indexing issues
-backButton = Button(universalFrame, text= "Back", width = 15, height = 2, command=lambda: loadAndUnload(previousWindow[-2]))
+backButton = Button(universalFrame, text= "Back", width = 15, height = 2, state='disabled', command=lambda: loadAndUnload(previousWindow[-2], True))
 backButton.grid(column=1, row= 0, padx= 5, pady = 5)
 
 settingsButton = Button(universalFrame, text= "Settings", width = 10, height = 2)
-settingsButton.grid(column=2, row= 0, padx= 5, pady = 5)
+#settingsButton.grid(column=2, row= 0, padx= 5, pady = 5)
 
 exitButton = Button(universalFrame, text= "Exit", width = 10, height = 2, command=window.quit)
 exitButton.grid(column=3, row= 0, padx= 5, pady = 5)
@@ -53,38 +65,42 @@ uploadMenu = Frame(window, bg= 'white')
 menuList.append(uploadMenu)
 
 uploadLabel = Label(uploadMenu, text = "Uploading and shit", width= 65, height=2, bg = "Coral")
-uploadLabel.grid(column = 0, row= 0, padx= 5, pady= 5, columnspan=2)
+uploadLabel.grid(column = 0, row= 0, padx= 5, pady= 5, columnspan=3)
 
-filename = ''
+filepath = ''
 def imageUpload():
-    global filename
+    global filepath
     Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
     filepath = askopenfilename() # show an "Open" dialog box and return the path to the selected file
     if filepath != '':
         #print(filepath)
         blurButton.config(state='normal')
         scrambleButton.config(state='normal')
+        negativeButton.config(state='normal')
         #make this a preview of upload
-        uploadedImageDisplay = Frame(uploadMenu, bg= 'aqua')
-        uploadedImageDisplay.grid(column= 1, row = 1, padx= 5, pady = 5)
-        fileNameLabel = Label(uploadedImageDisplay, text = filepath[:15] + '...' + filepath[-15:-1])
-        fileNameLabel.grid(column= 0, row = 0)
+
+        fileNameLabel = Label(uploadMenu, text = filepath[:20] + '...' + filepath[-20:-1] + filepath[-1])
+        fileNameLabel.grid(column= 1, row = 1, columnspan=2)
+        '''
         im = ImageTk.PhotoImage(Image.open(filepath))
         thumbNailIm = Canvas(uploadedImageDisplay, height= 100, width= 100)
         thumbNailIm.create_image(20, 20, image = im)
         thumbNailIm.grid(column= 0, row = 1)
+        '''
 
 #set up command to get a file path to work with, have this function enable the blurbutton and scramble button
 uploadImageButton = Button(uploadMenu, text= "Upload an Image", width = 15, height = 2, command=imageUpload)
 uploadImageButton.grid(column= 0, row = 1, padx= 5, pady = 5)
 
 #hook this up to the blur func and pass argument of file path, enable when you get a file path
-blurButton = Button(uploadMenu, text= "Blur this image", state="disabled", width = 20, height = 4, command= lambda:Blur(filename))
+blurButton = Button(uploadMenu, text= "Blur this image", state="disabled", width = 15, height = 4, command= lambda:Blur(filepath))
 blurButton.grid(column= 0, row = 2, padx= 5, pady = 5)
 
-scrambleButton = Button(uploadMenu, text= "Scramble this image", state="disabled", width = 20, height = 4, command= lambda:Scrambler(filename))
+scrambleButton = Button(uploadMenu, text= "Scramble this image", state="disabled", width = 15, height = 4, command= lambda:Scrambler(filepath))
 scrambleButton.grid(column= 1, row = 2, padx= 5, pady = 5)
 
+negativeButton = Button(uploadMenu, text= "Make a negative", state="disabled", width = 15, height = 4, command= lambda:makeNegative(filepath))
+negativeButton.grid(column= 2, row = 2, padx= 5, pady = 5)
 
 createMenu = Frame(window, bg= 'white')
 menuList.append(createMenu)
@@ -95,22 +111,41 @@ createButton.grid(column = 1, row= 1, padx= 5)
 createLabel = Label(createMenu, text = "Creating and shit", width= 65, height= 2, bg = "coral")
 createLabel.grid(column = 0, row= 0, padx= 5, pady = 5, columnspan=2)
 
-numLabel = Label(createMenu, text= "Enter the size of square you would like to generate")
-numLabel.grid(column= 0, row = 1, padx= 5, pady = 5, columnspan=2)
+individualNoiseMenu = Frame(window)
+menuList.append(individualNoiseMenu)
 
-numField = Entry(createMenu)
-numField.grid(column= 0, row = 2, padx= 5, pady = 5, columnspan=2)
+individualButton = Button(createMenu,text = "Individual Noise", width=20, height= 2, command=lambda:loadAndUnload(individualNoiseMenu))
+individualButton.grid(column= 0, row = 1, padx= 5, pady = 5)
 
-numWarningLabel = Label(createMenu, text= "That is not a number")
+indivLabel = Label(individualNoiseMenu, text = "Indivual Noise Generation", width= 65, height= 2, bg = "coral")
+indivLabel.grid(column = 0, row= 0, padx= 5, pady = 5, columnspan=2)
 
-def testInput(numFieldInput):
-    if(numFieldInput.isdecimal()):
-        NoiseGenerator(int(numFieldInput), rgb)
-        print(rgb)
+inNumLabel = Label(individualNoiseMenu, text= "Enter the size of square you would like to generate")
+inNumLabel.grid(column= 0, row = 1, padx= 5, pady = 5, columnspan=2)
+
+inNumField = Entry(individualNoiseMenu)
+inNumField.grid(column= 0, row = 2, padx= 5, pady = 5, columnspan=2)
+
+inNumWarningLabel = Label(individualNoiseMenu, fg = 'red', text= "That is not a number")
+
+def testInput(mode, numFieldInput):
+    if(mode == 'indiv'):
+        if(numFieldInput.isdecimal()):
+            NoiseGenerator(int(numFieldInput), rgb)
+        else:
+            inNumWarningLabel.grid(column= 0, row = 3, padx= 5, pady = 5, columnspan=2)
     else:
-        numWarningLabel.grid(column= 0, row = 3, padx= 5, pady = 5, columnspan=2)
-
-bwRgbLabel = Label(createMenu, text= "Would you like Black and White Noise or Noise with all colors?")
+        inputList = numFieldInput.split(",")
+        allInt = True
+        for num in inputList:
+            if(num.isdecimal() != True or len(inputList) != 3):
+                allInt = False
+                clusNumWarningLabel.grid(column= 0, row = 3, padx= 5, pady = 5, columnspan=2)
+        if(allInt):
+            print(inputList[0], inputList[1], inputList[2])
+            NoiseClusters(int(inputList[0]), int(inputList[1]), int(inputList[2]))
+        
+bwRgbLabel = Label(individualNoiseMenu, text= "Would you like Black and White Noise or Noise with all colors?")
 bwRgbLabel.grid(column= 0, row = 4, padx= 5, pady = 5, columnspan=2)
 
 rgb = False
@@ -126,13 +161,35 @@ def switchBwRgb(mode):
         rgbButton.config(state= 'normal')
         rgb = False
 
-bwButton = Button(createMenu, text= "Black and White", width = 15, height = 2, state='disabled', command= lambda: switchBwRgb('bw'))
+bwButton = Button(individualNoiseMenu, text= "Black and White", width = 15, height = 2, state='disabled', command= lambda: switchBwRgb('bw'))
 bwButton.grid(column= 0, row = 5, padx= 5, pady = 5)
 
-rgbButton = Button(createMenu, text= "Rgb", width = 15, height = 2, command= lambda: switchBwRgb('rgb'))
+rgbButton = Button(individualNoiseMenu, text= "Rgb", width = 15, height = 2, command= lambda: switchBwRgb('rgb'))
 rgbButton.grid(column= 1, row = 5, padx= 5, pady = 5)
 
-noiseButton = Button(createMenu, text= "Make Some Noise", width = 60, height = 4, command= lambda: testInput(numField.get()))
-noiseButton.grid(column= 0, row = 6, padx= 5, pady = 5, columnspan=2)
+inNoiseButton = Button(individualNoiseMenu, text= "Make Some Noise", width = 60, height = 4, command= lambda: testInput('indiv', inNumField.get()))
+inNoiseButton.grid(column= 0, row = 6, padx= 5, pady = 5, columnspan=2)
+
+#-------------------------------------------------
+clusterNoiseMenu = Frame(window)
+menuList.append(clusterNoiseMenu)
+
+clusterButton = Button(createMenu,text = "Clustered Noise", width=20,  height= 2, command=lambda:loadAndUnload(clusterNoiseMenu))
+clusterButton.grid(column= 1, row = 1, padx= 5, pady = 5)
+
+clusterLabel = Label(clusterNoiseMenu, text = "Cluster Noise Generation", width= 65, height= 2, bg = "coral")
+clusterLabel.grid(column = 0, row= 0, padx= 5, pady = 5, columnspan=2)
+
+clusnumLabel = Label(clusterNoiseMenu, text= "Enter Values seperated by commas in the respective order:\n Square Size, Cluster Radius, Number of Clusters ex:(100,10,40)")
+clusnumLabel.grid(column= 0, row = 1, padx= 5, pady = 5, columnspan=2)
+
+clusNumField = Entry(clusterNoiseMenu)
+clusNumField.grid(column= 0, row = 2, padx= 5, pady = 5, columnspan=2)
+
+clusNumWarningLabel = Label(clusterNoiseMenu, fg = 'red', text= "There was an issue with your input, make sure there are no spaces\n and there are three elements seperated by commas")
+
+clusNoiseButton = Button(clusterNoiseMenu, text= "Make Some Noise", width = 60, height = 4, command= lambda: testInput('clus', clusNumField.get()))
+clusNoiseButton.grid(column= 0, row = 5, padx= 5, pady = 5, columnspan=2)
+#-------------------------------------------------
 
 window.mainloop()
